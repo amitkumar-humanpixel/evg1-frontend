93de4151f4d0a3624356e2a26650f2cceb544f27
Mark Allen <mark.allen@evgptraining.com.au>
2021-09-13 11:19:56 +1000

    This could potentially expose an Okta Secret publically, and is not required for proper operation of the frontend SPA

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const oktaConfig = {
   issuer: process.env.REACT_APP_ISSUER,
   clientId: process.env.REACT_APP_CLIENT_ID,
-  client_secret: process.env.REACT_APP_CLIENT_SECRET,
   appBaseUrl: process.env.REACT_APP_OCTA_BASE_URI,
   redirectUri: `${process.env.REACT_APP_OCTA_BASE_URI}/callback`,
   loginRedirectionUri: `${process.env.REACT_APP_OCTA_BASE_URI}/callback`,
