2b7e0318671278e3295d61bfa2b5d1decab48f7c
Mark Allen <mark.allen@evgptraining.com.au>
2021-10-04 12:24:10 +1100

    Update Text for Reaccreditation Checklist, and Remove Checklist itself

--- src/Pages/Accredited/ReaccreditationChecklist/ReaccreditationChecklist.js
+++ src/Pages/Accredited/ReaccreditationChecklist/ReaccreditationChecklist.js
@@ -1,35 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useQueryParams } from '../../../hooks/GetQueryParamHook';
-import { AccreditedEditableHelper } from '../../../helpers/AccreditedEditableHelper';
-import TriStateSwitch from '../../../components/TriStateSwitch/TriStateSwitch';
-import Checklist from '../Checklist';
-import { getReAccreditedCheckList, updateAccreditedSubFormArrayFields } from '../redux/AccreditedReduxActions';
+import { getReAccreditedCheckList } from '../redux/AccreditedReduxActions';
 
 const ReaccreditationChecklist = () => {
   const { id } = useQueryParams();
   const dispatch = useDispatch();
-  const { step, subStep } = useParams();
-  const [isEditable, setIsEditable] = useState(true);
 
-  const { accreditionSideBar } = useSelector(({ accreditedReducer }) => accreditedReducer?.accreditedStepper ?? {});
-
-  const { reaccreditationChecklist } = useSelector(
-    ({ accreditedReducer }) => accreditedReducer?.reaccreditationChecklist ?? {},
-  );
-
-  const handleCheckInputChange = useCallback(
-    (index, name, value) =>
-      dispatch(
-        updateAccreditedSubFormArrayFields('reaccreditationChecklist', 'reaccreditationChecklist', index, name, value),
-      ),
-    [],
-  );
-
-  useEffect(() => {
-    setIsEditable(AccreditedEditableHelper(accreditionSideBar, step, subStep));
-  }, [step, subStep, accreditionSideBar]);
+  const { facilityName } = useSelector(({ accreditedReducer }) => accreditedReducer?.postDetails ?? {});
 
   useEffect(() => {
     if (id) dispatch(getReAccreditedCheckList(id));
@@ -38,34 +16,74 @@ const ReaccreditationChecklist = () => {
   return (
     <>
       <section>
-        <div className="accredited-title mb-10">Introduction</div>
-        <div className="common-white-container" style={{ 'margin-bottom': '20px' }}>
-          <div className="standard-texts">
-            Supervisors and practice managers are referred to the most recent EV Training Post Handbook for guidance on
-            current standards for teaching and supervision plus administrative requirements. If not already received The
-            Training post handbook is available in electronic form from the Accreditation Support Administrator at
-            EVGPT. Handbooks and Quick Guides are also available from the EV website at{' '}
-            <a href="https://www.evgptraining.com.au/resources/" target="_blank" rel="noreferrer">
-              https://www.evgptraining.com.au/resources/
-            </a>
-            .
-          </div>
-        </div>
-        <div className="accredited-title mb-10">Checklist</div>
+        <div className="accredited-title mb-10">Reaccreditation Application – {facilityName}</div>
         <div className="common-white-container">
-          {reaccreditationChecklist?.map((detail, index) => (
-            <div className="final-checklist">
-              <TriStateSwitch
-                state={detail?.status}
-                onChange={state => handleCheckInputChange(index, 'status', state)}
-                disabled={!isEditable}
-              />
-              <div>
-                <Checklist detail={detail?.title} />
-                {detail?.error && <div className="form-error-message">{detail?.error}</div>}
-              </div>
-            </div>
-          ))}
+          <div className="standard-texts">
+            <p>
+              Supervisors and practice managers are referred to the most recent EV Training Post Handbook for guidance
+              on current standards for teaching and supervision plus administrative requirements. If not already
+              received The Training post handbook is available in electronic form from the Accreditation Support
+              Administrator at EVGPT. Handbooks and Quick Guides are also available from the EV website at{' '}
+              <a href="https://www.evgptraining.com.au/resources/" target="_blank" rel="noreferrer">
+                https://www.evgptraining.com.au/resources/
+              </a>
+              .
+            </p>
+
+            <p>
+              Please see instructions below about your reaccreditation application and the documents and information you
+              will need to complete it.
+            </p>
+            <ol>
+              <li>
+                Documents below will need to be available to electronically attach when submitting the application and
+                actions below will need to be completed. There will be a separate checkbox at the end of this document
+                that you can fill in to tick the completion off – but this is here to help you prepare.
+              </li>
+              <li>
+                First section (now called Practice Manager Information) to be completed by the Practice Manager. This
+                includes information about your working days, the operating hours of the practice, the facility
+                accreditation (by AGPAL or QPA or other), some statements about the facility and some basic information
+                relating to each supervisor. This includes rostered hours for each supervisor and registrar.
+              </li>
+              <li>
+                Please note that all information on the following pages must be completed for you to submit the
+                application, but you can proceed through the form to the next section by pressing Save to retain your
+                answers so far and then Next at the bottom right hand of the page.
+              </li>
+              <li>
+                When Practice Manager’s section is submitted, a separate declaration called Supervisor Information is
+                sent to each named supervisor. This applies only to currently accredited supervisors seeking
+                reaccreditation. New supervisors apply separately.
+              </li>
+              <li>
+                Each supervisor must fill in their declaration and answer the necessary declarations and submit. When
+                these have all been completed by each reaccrediting supervisor, the Practice Manager and/or Principal
+                Educational Supervisor will be able to complete the Final Application Submission.
+              </li>
+              <li>
+                The Final Application Submission has the checklist to assist identifying completion of each section and
+                also provides the practice response to any previous recommendations that have been provided to the
+                practice. This may have been at the initial accreditation of the practice, a reaccreditation, or a
+                review visit. These will be incorporated into the application in future iterations, but for now, have
+                been provided to the practice manager and/or principal supervisor directly by email.
+              </li>
+            </ol>
+            <p>Things you will need to submit your application</p>
+            <ol>
+              <li>
+                All answers in each section should be completed by the appropriate named user and submission by that
+                user indicates that the information is true and correct – so is deemed to be a signed declaration.
+              </li>
+              <li>
+                A copy of a current accreditation certificate (AGPAL, QPA or other). This is not required for ACRRM only
+                accreditation (Rural).
+              </li>
+              <li>A copy of an employment contract template used for registrars</li>
+              <li>A copy of a registrar specific orientation checklist as used by the practice</li>
+              <li>Where supervisors have hospital clinical privileges, a letter confirming this is required.</li>
+            </ol>
+          </div>
         </div>
       </section>
     </>
--- src/Pages/Accredited/ReaccreditationChecklist/ReaccreditationChecklistValidations.js
+++ src/Pages/Accredited/ReaccreditationChecklist/ReaccreditationChecklistValidations.js
@@ -1,4 +1,3 @@
-import { saveReAccreditedCheckList, updateAccreditedSubFormArrayFields } from '../redux/AccreditedReduxActions';
 import { setNextAccreditedItemUrl } from '../../../helpers/AccreditedGoToNextStepHelper';
 
 export const ReaccreditationChecklistValidations = async (
@@ -13,41 +12,10 @@ export const ReaccreditationChecklistValidations = async (
   step,
   subStep,
 ) => {
-  let validated = true;
-
-  const finalData = {
-    facilityId,
-    reaccreditationChecklist: data?.reaccreditationChecklist,
-  };
-
-  finalData?.reaccreditationChecklist?.forEach((point, index) => {
-    if (!point?.status || point?.status === 'none') {
-      validated = false;
-      dispatch(
-        updateAccreditedSubFormArrayFields(
-          'reaccreditationChecklist',
-          'reaccreditationChecklist',
-          index,
-          'error',
-          'Please select suitable status for this point',
-        ),
-      );
-    } else {
-      dispatch(
-        updateAccreditedSubFormArrayFields(
-          'reaccreditationChecklist',
-          'reaccreditationChecklist',
-          index,
-          'error',
-          undefined,
-        ),
-      );
-    }
-  });
+  const validated = true;
 
   if (validated) {
     try {
-      await dispatch(saveReAccreditedCheckList(finalData, accreditionId));
       if (isNextClick) setNextAccreditedItemUrl(history, accreditionSideBar, accreditionId, step, subStep);
       // history.push(`/accredited/formB/?id=${id}`);
     } catch (e) {
